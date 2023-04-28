bafa2daacf9a5bb582add3467baa39b546be78d0
jeffasante <jeffreyoduro@icloud.com>
2023-04-27 20:07:44 -0700

    added footer tempplate

--- Assets /Js/Custom.js
+++ Assets /Js/Custom.js
@@ -10,9 +10,20 @@ let value = 50;
 valueOutput.innerHTML = "$" + (value * 100).toFixed(2);
 
 // Create the slider and listen for changes to the slider value
-slider.addEventListener("input", function(event) {
+slider.addEventListener("input", function (event) {
   // Update the money value based on the current slider value
   const currentValue = event.target.value;
   const price = currentValue * 20;
   valueOutput.innerHTML = "$" + price.toFixed(2);
 });
+
+
+// reusable footer
+
+const footer = document.getElementById(".footer");
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "/footer.html");
+xhr.onload = function () {
+  footer.innerHTML = xhr.responseText;
+};
+xhr.send();
