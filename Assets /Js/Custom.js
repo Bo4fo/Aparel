cacefcfe13d596e8d1d73cf6fabf556acc00c907
Kboafo <mac@macs-MacBook-Pro.local>
2023-05-05 15:14:36 +0000

    6th

--- Assets /Js/Custom.js
+++ Assets /Js/Custom.js
@@ -1,7 +1,35 @@
-  // JavaScript code for mobile view functionality
-  const mobileToggle = document.querySelector('.navbar-toggler');
-  const mobileMenu = document.querySelector('.navbar-collapse');
+// JavaScript code for mobile view functionality
+const mobileToggle = document.querySelector('.navbar-toggler');
+const mobileMenu = document.querySelector('.navbar-collapse');
 
-  mobileToggle.addEventListener('click', function () {
-      mobileMenu.classList.toggle('show');
-  });
+mobileToggle.addEventListener('click', function () {
+    mobileMenu.classList.toggle('show');
+});
+
+document.addEventListener('click', function (event) {
+    const targetElement = event.target;
+    if (!targetElement.closest('.navbar-collapse') && !targetElement.closest('.navbar-toggler')) {
+        mobileMenu.classList.remove('show');
+    }
+});
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Open the modal
+    function openModal() {
+      modal.style.display = "block";
+    }
+
+    // Close the modal
+    function closeModal() {
+      modal.style.display = "none";
+    }
+
+    // Add to cart
+    function addToCart() {
+      var quantity = document.getElementById("quantitySelect").value;
+      // Perform add to cart functionality
+      // You can send an AJAX request to the server here to add the item to the cart
+      console.log("Added " + quantity + " items to cart.");
+      closeModal();
+    }
